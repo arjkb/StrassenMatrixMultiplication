ea04ae7af9e491e245344edeb42949eb85d50163
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-07-18 18:51:39 +0530

    working implementation of str_to_int()

--- strassens_mm.c
+++ strassens_mm.c
@@ -2,7 +2,34 @@
   Arjun Krishna Babu
 */
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int str_to_int(const char *numstring);
+
 int main()  {
   printf(" Hello, world!\n");
+  
   return 0;
+}
+
+
+int str_to_int(const char *numstring) {
+  int number = 0;
+  int i;
+  int digit = 0;
+
+  for(i = 0; numstring[i] != '\0'; i++) {
+    if( isdigit(numstring[i]) ) {
+      digit = numstring[i] - '0';
+      number = (number * 10) + digit;
+      // printf(" DEBUG: Number: %d, %d %d\n", i, number, digit);
+    }
+    else  {
+      printf(" Error: str_to_int(%s): not a digit\n", numstring);
+      return 0;
+    }
+  }
+  return number;
 }
